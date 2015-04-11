e1f59fa90f09dca89eaaa5fc5507449695cef326
Tatiana Cohen <tatiana.cohen.arias@gmail.com>
2015-04-10 20:30:32 -0400

    installed second plugin and last updates

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
+URL: http://tatianacohen.com/resume/
+
+Plugins:
 https://github.com/heelhook/chardin.js
+http://markgoodyear.com/2013/01/scrollup-jquery-plugin/
--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ $( document ).ready(function() {
 	  $( 'body' ).chardinJs('start');
 	});
 
-
+    $(function () {
+	  $.scrollUp({
+	    scrollName: 'scrollUp', 
+	    topDistance: '400', 
+	    topSpeed: 300, 
+	    animation: 'fade', 
+	    animationInSpeed: 200, 
+	    animationOutSpeed: 200, 
+	    scrollText: 'Scroll to top', 
+	    activeOverlay: false, 
+	  });
+	});
 
 
 });
